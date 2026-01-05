90555332f9a3e98c173ee4d8db5fa672f40df7ad
Gilles Roustan <gilles.roustan@mediapart.fr>
2026-01-05 08:44:12 +0100

    fix: agent path

--- functions/fish_ssh_agent.fish
+++ functions/fish_ssh_agent.fish
@@ -1,78 +1,88 @@
 function sshagent_findsockets
-   find /tmp -uid (id -u) -type s -name agent.\* 2>/dev/null
+    find $HOME/.ssh/agent/s.* -uid (id -u) -type s -name \*.agent.\* 2>/dev/null
 end
 
 function sshagent_testsocket
-   if [ ! -x (command which ssh-add) ] ;
-      echo "ssh-add is not available; agent testing aborted"
-      return 1
-   end
+    if [ ! -x (command which ssh-add) ]
 
-   if [ X"$argv[1]" != X ] ;
-      set -xg SSH_AUTH_SOCK $argv[1]
-   end
+        echo "ssh-add is not available; agent testing aborted"
+        return 1
+    end
 
-   if [ X"$SSH_AUTH_SOCK" = X ]
-      return 2
-   end
+    if [ X"$argv[1]" != X ]
 
-   if [ -S $SSH_AUTH_SOCK ] ;
-      ssh-add -l > /dev/null
-      if [ $status = 2 ] ;
-         echo "Socket $SSH_AUTH_SOCK is dead!  Deleting!"
-         rm -f $SSH_AUTH_SOCK
-         return 4
-      else ;
-         echo "Found ssh-agent $SSH_AUTH_SOCK"
-         return 0
-      end
-   else ;
-      echo "$SSH_AUTH_SOCK is not a socket!"
-      return 3
-   end
+        set -xg SSH_AUTH_SOCK $argv[1]
+    end
+
+    if [ X"$SSH_AUTH_SOCK" = X ]
+        return 2
+    end
+
+    if [ -S $SSH_AUTH_SOCK ]
+
+        ssh-add -l >/dev/null
+        if [ $status = 2 ]
+
+            echo "Socket $SSH_AUTH_SOCK is dead!  Deleting!"
+            rm -f $SSH_AUTH_SOCK
+            return 4
+        else
+
+            echo "Found ssh-agent $SSH_AUTH_SOCK"
+            return 0
+        end
+    else
+
+        echo "$SSH_AUTH_SOCK is not a socket!"
+        return 3
+    end
 end
-
 
 function fish_ssh_agent
-   # ssh agent sockets can be attached to a ssh daemon process or an
-   # ssh-agent process.
+    # ssh agent sockets can be attached to a ssh daemon process or an
+    # ssh-agent process.
 
-   set -l AGENTFOUND 0
+    set -l AGENTFOUND 0
 
-   # Attempt to find and use the ssh-agent in the current environment
-   if sshagent_testsocket ;
-      set AGENTFOUND 1
-   end
+    # Attempt to find and use the ssh-agent in the current environment
+    if sshagent_testsocket
 
-   # If there is no agent in the environment, search /tmp for
-   # possible agents to reuse before starting a fresh ssh-agent
-   # process.
-   if [ $AGENTFOUND = 0 ];
-      for agentsocket in (sshagent_findsockets)
-         if [ $AGENTFOUND != 0 ] ;
-            break
-         end
-         if sshagent_testsocket $agentsocket ;
-            set AGENTFOUND 1
-         end
+        set AGENTFOUND 1
+    end
 
-      end
-   end
+    # If there is no agent in the environment, search $HOME/.ssh/agent/s.*for
+    # possible agents to reuse before starting a fresh ssh-agent
+    # process.
+    if [ $AGENTFOUND = 0 ]
 
-   # If at this point we still haven't located an agent, it's time to
-   # start a new one
-   if [ $AGENTFOUND = 0 ] ;
-      echo need to start a new agent
-      eval (ssh-agent -c)
-   end
+        for agentsocket in (sshagent_findsockets)
+            if [ $AGENTFOUND != 0 ]
 
-   # Finally, show what keys are currently in the agent
-   ssh-add -l > /dev/null
-   if [ $status = 1 ] ;
-      for key in  id_rsa id_dsa id_ecdsa id_ed25519 identity
-         test -f $HOME/.ssh/$key &&
-         ssh-add $HOME/.ssh/$key
-      end
-   end
+                break
+            end
+            if sshagent_testsocket $agentsocket
+
+                set AGENTFOUND 1
+            end
+
+        end
+    end
+
+    # If at this point we still haven't located an agent, it's time to
+    # start a new one
+    if [ $AGENTFOUND = 0 ]
+
+        echo need to start a new agent
+        eval (ssh-agent -c)
+    end
+
+    # Finally, show what keys are currently in the agent
+    ssh-add -l >/dev/null
+    if [ $status = 1 ]
+
+        for key in id_rsa id_dsa id_ecdsa id_ed25519 identity
+            test -f $HOME/.ssh/$key &&
+                ssh-add $HOME/.ssh/$key
+        end
+    end
 end
-
